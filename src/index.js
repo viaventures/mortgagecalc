50127290c43a05371091c56e84013cfe84b19688
Tony Anglesey <ta@vvinc.co>
2024-03-19 18:55:36 -0500

    set up as PWA

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import App from './App'
 import { ThemeProvider } from '@mui/material/styles'
 import CssBaseline from '@mui/material/CssBaseline'
 import { theme } from './theme'
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -14,3 +15,8 @@ root.render(
     </ThemeProvider>
   </React.StrictMode>
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
